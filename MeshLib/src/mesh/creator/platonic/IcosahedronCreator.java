8f973fe746c05b75814368f49f657e6123af9ec7
Simon <Simon@LAPTOP-NAT2JJES.Speedport_W_921V_1_45_000>
2019-03-16 06:41:28 +0100

    Changed implementation of IcosahedronCreator and added a test class.

--- MeshLib/src/mesh/creator/platonic/IcosahedronCreator.java
+++ MeshLib/src/mesh/creator/platonic/IcosahedronCreator.java
@@ -1,72 +1,63 @@
 package mesh.creator.platonic;
 
-import math.Vector3f;
-import mesh.Face3D;
+import math.Mathf;
 import mesh.Mesh3D;
 import mesh.creator.IMeshCreator;
 
 public class IcosahedronCreator implements IMeshCreator {
 
-	private static final float[][] VERTICES = {
-			{ 0.000000f, -1.000000f, 0.000000f },
-			{ 0.723600f, -0.447215f, 0.525720f },
-			{ -0.276385f, -0.447215f, 0.850640f },
-			{ -0.894425f, -0.447215f, 0.000000f },
-			{ -0.276385f, -0.447215f, -0.850640f },
-			{ 0.723600f, -0.447215f, -0.525720f },
-			{ 0.276385f, 0.447215f, 0.850640f },
-			{ -0.723600f, 0.447215f, 0.525720f },
-			{ -0.723600f, 0.447215f, -0.525720f },
-			{ 0.276385f, 0.447215f, -0.850640f },
-			{ 0.894425f, 0.447215f, -0.000000f },
-			{ 0.000000f, 1.000000f, -0.000000f }, };
+	private static final float GOLDEN_RATIO = 0.5f * (1 + Mathf.sqrt(5));
 
-	private static final int[][] FACES = { { 2, 0, 1 }, { 1, 0, 5 },
-			{ 3, 0, 2 }, { 4, 0, 3 }, { 5, 0, 4 }, { 1, 5, 10 }, { 2, 1, 6 },
-			{ 3, 2, 7 }, { 4, 3, 8 }, { 5, 4, 9 }, { 10, 6, 1 }, { 6, 7, 2 },
-			{ 7, 8, 3 }, { 8, 9, 4 }, { 9, 10, 5 }, { 6, 10, 11 },
-			{ 7, 6, 11 }, { 8, 7, 11 }, { 9, 8, 11 }, { 10, 9, 11 }, };
-
-	private float size;
 	private Mesh3D mesh;
 
-	public IcosahedronCreator() {
-		this.size = 1f;
-	}
-
-	public IcosahedronCreator(float size) {
-		this.size = size;
-	}
-
 	private void createVertices() {
-		for (int i = 0; i < VERTICES.length; i++) {
-			mesh.add(new Vector3f(VERTICES[i][0], VERTICES[i][1],
-					VERTICES[i][2]));
-		}
+		mesh.addVertex(0, -1, -GOLDEN_RATIO);
+		mesh.addVertex(0, 1, -GOLDEN_RATIO);
+		mesh.addVertex(0, -1, GOLDEN_RATIO);
+		mesh.addVertex(0, 1, GOLDEN_RATIO);
+		mesh.addVertex(-1, -GOLDEN_RATIO, 0);
+		mesh.addVertex(1, -GOLDEN_RATIO, 0);
+		mesh.addVertex(-1, GOLDEN_RATIO, 0);
+		mesh.addVertex(1, GOLDEN_RATIO, 0);
+		mesh.addVertex(-GOLDEN_RATIO, 0, -1);
+		mesh.addVertex(-GOLDEN_RATIO, 0, 1);
+		mesh.addVertex(GOLDEN_RATIO, 0, -1);
+		mesh.addVertex(GOLDEN_RATIO, 0, 1);
 	}
 
 	private void createFaces() {
-		for (int i = 0; i < FACES.length; i++) {
-			mesh.add(new Face3D(FACES[i][0], FACES[i][1],
-					FACES[i][2]));
-		}
+		mesh.addFace(4, 5, 2);
+		mesh.addFace(5, 11, 2);
+		mesh.addFace(5, 10, 11);
+		mesh.addFace(5, 0, 10);
+		mesh.addFace(5, 4, 0);
+		mesh.addFace(4, 8, 0);
+		mesh.addFace(4, 9, 8);
+		mesh.addFace(4, 2, 9);
+		mesh.addFace(0, 8, 1);
+		mesh.addFace(0, 1, 10);
+		mesh.addFace(10, 1, 7);
+		mesh.addFace(11, 10, 7);
+		mesh.addFace(2, 11, 3);
+		mesh.addFace(9, 2, 3);
+		mesh.addFace(9, 3, 6);
+		mesh.addFace(8, 9, 6);
+		mesh.addFace(8, 6, 1);
+		mesh.addFace(11, 7, 3);
+		mesh.addFace(3, 7, 6);
+		mesh.addFace(6, 7, 1);
+	}
+
+	private void initializeMesh() {
+		mesh = new Mesh3D();
 	}
 
 	@Override
 	public Mesh3D create() {
-		mesh = new Mesh3D();
+		initializeMesh();
 		createVertices();
 		createFaces();
-		mesh.scale(size);
 		return mesh;
-	}
-
-	public float getSize() {
-		return size;
-	}
-
-	public void setSize(float size) {
-		this.size = size;
 	}
 
 }
---
+++ MeshLib/test/mesh/creator/platonic/IcosahedronCreatorTest.java
@@ -0,0 +1,50 @@
+package mesh.creator.platonic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import mesh.Mesh3D;
+import mesh.creator.IMeshCreator;
+import util.MeshTestUtil;
+
+public class IcosahedronCreatorTest {
+
+	IcosahedronCreator creator = new IcosahedronCreator();
+	Mesh3D mesh = creator.create();
+
+	@Test
+	public void creatorImplementsMeshCreatorInterface() {
+		Assert.assertTrue(creator instanceof IMeshCreator);
+	}
+	
+	@Test
+	public void createdMeshHasTwentyFaces() {
+		Assert.assertEquals(20, mesh.getFaceCount());
+	}
+	
+	@Test
+	public void createdMesHasTwelveVertices() {
+		Assert.assertEquals(12, mesh.getVertexCount());
+	}
+	
+	@Test
+	public void createdMeshHasThirtyEdges() {
+		Assert.assertEquals(30, mesh.createEdges().size());		
+	}
+	
+	@Test
+	public void createdMeshFulFillsEulersCharacteristic() {
+		MeshTestUtil.meshFulFillsEulersCharacteristic(mesh);
+	}
+	
+	@Test
+	public void eachEdgeOfTheCreatedMeshIsIncidentToOnlyOneOrTwoFaces() {
+		MeshTestUtil.eachEdgeIsIncidentToOnlyOneOrTwoFaces(mesh);
+	}
+	
+	@Test
+	public void allEdgesOfTheCreatedMeshHaveEqualLength() {
+		MeshTestUtil.allEdgesHaveEqualLength(mesh);
+	}
+
+}
