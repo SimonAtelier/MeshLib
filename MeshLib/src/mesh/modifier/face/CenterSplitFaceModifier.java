b9ab4e5d295c6e2e2ee027249e2ff218bdad6c6d
Simon <Simon@192.168.2.104>
2019-03-21 11:51:22 +0100

    Refactoring

--- MeshLib/src/mesh/modifier/face/CenterSplitFaceModifier.java
+++ MeshLib/src/mesh/modifier/face/CenterSplitFaceModifier.java
@@ -11,24 +11,56 @@ import mesh.wip.Mesh3DUtil;
 
 public class CenterSplitFaceModifier {
 
+	private Mesh3D mesh;
+	private FaceSelection selection;
+	private List<Face3D> newFaces;
+
+	public CenterSplitFaceModifier() {
+		newFaces = new ArrayList<Face3D>();
+	}
+	
 	public void modify(FaceSelection selection) {
+		clearNewlyCreatedFaces();
+		setMesh(selection.getMesh());
+		setSelection(selection);
+		centerSplitSelectedFaces();
+		addNewlyCreatedFaces();
+	}
+	
+	private void centerSplitSelectedFaces() {
 		for (Face3D face : selection.getFaces()) {
-			centerSplit(selection.getMesh(), face);
+			centerSplit(face);
 		}
 	}
+
+	private void clearNewlyCreatedFaces() {
+		newFaces.clear();
+	}
 	
-	private void centerSplit(Mesh3D mesh, Face3D f) {
-		int index = mesh.getVertexCount();
-		int n = f.indices.length;
-		List<Face3D> toAdd = new ArrayList<Face3D>();
-		Vector3f center = Mesh3DUtil.calculateFaceCenter(mesh, f);
+	private void addNewlyCreatedFaces() {
+		mesh.faces.addAll(newFaces);
+	}
+
+	private void centerSplit(Face3D face) {
+		int nextIndex = mesh.getVertexCount();
+		int indicesLength = face.indices.length;
+		Vector3f center = Mesh3DUtil.calculateFaceCenter(mesh, face);
 		mesh.add(center);
-		for (int i = 0; i < f.indices.length; i++) {
-			Face3D f1 = new Face3D(f.indices[i % n], f.indices[(i + 1) % n], index);
-			toAdd.add(f1);
+		for (int i = 0; i < face.indices.length; i++) {
+			int index0 = face.indices[i % indicesLength];
+			int index1 =  face.indices[(i + 1) % indicesLength];
+			int index3 = nextIndex;
+			newFaces.add(new Face3D(index0, index1, index3));
 		}
-		mesh.faces.addAll(toAdd);
-		mesh.faces.remove(f);
+		mesh.faces.remove(face);
 	}
 	
+	private void setMesh(Mesh3D mesh) {
+		this.mesh = mesh;
+	}
+	
+	private void setSelection(FaceSelection selection) {
+		this.selection = selection;
+	}
+
 }
