11ea36082f3e9436480fc64579e43173dffa73c3
Simon <Simon@LAPTOP-NAT2JJES.Speedport_W_921V_1_45_000>
2019-03-23 16:13:11 +0100

    Moved method to mesh class.

--- MeshLib/src/mesh/Mesh3D.java
+++ MeshLib/src/mesh/Mesh3D.java
@@ -9,6 +9,7 @@ import java.util.List;
 import math.Mathf;
 import math.Matrix3f;
 import math.Vector3f;
+import mesh.math.Bounds3;
 
 public class Mesh3D {
 
@@ -196,6 +197,22 @@ public class Mesh3D {
 			center.addLocal(vertices.get(face.indices[i]));
 		}
 		return center.divideLocal(face.indices.length);
+	}
+	
+	public Bounds3 calculateBounds() {
+		Vector3f min = new Vector3f();
+		Vector3f max = new Vector3f();
+		Bounds3 bounds = new Bounds3();
+		for (Vector3f v : vertices) {
+			min.x = v.x < min.x ? v.x : min.x;
+			min.y = v.y < min.y ? v.y : min.y;
+			min.z = v.z < min.z ? v.z : min.z;
+			max.x = v.x > max.x ? v.x : max.x;
+			max.y = v.y > max.y ? v.y : max.y;
+			max.z = v.z > max.z ? v.z : max.z;
+		}
+		bounds.setMinMax(min, max);
+		return bounds;
 	}
 	
 	public List<Face3D> getFaces(int from, int to) {
--- MeshLib/src/mesh/wip/Mesh3DOperations.java
+++ MeshLib/src/mesh/wip/Mesh3DOperations.java
@@ -6,20 +6,6 @@ import mesh.math.Bounds3;
 
 public class Mesh3DOperations {
 
-	public static Bounds3 getBounds(Mesh3D mesh) {
-		Vector3f min = new Vector3f();
-		Vector3f max = new Vector3f();
-		Bounds3 bounds = new Bounds3();
-		for (Vector3f v : mesh.vertices) {
-			min.x = v.x < min.x ? v.x : min.x;
-			min.y = v.y < min.y ? v.y : min.y;
-			min.z = v.z < min.z ? v.z : min.z;
-			max.x = v.x > max.x ? v.x : max.x;
-			max.y = v.y > max.y ? v.y : max.y;
-			max.z = v.z > max.z ? v.z : max.z;
-		}
-		bounds.setMinMax(min, max);
-		return bounds;
-	}
+
 	
 }
